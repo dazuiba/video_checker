574bbdd8ec2e50c1584e12f4258eb0473146f0f3
dazuiba <come2u@gmail.com>
2010-06-03 19:39:41 +0800

    some bugfix

--- use_video_checker.rb
+++ use_video_checker.rb
@@ -1,4 +1,4 @@
 require 'video_checker'
 urls = File.read("ok_urls.txt").split
-checker = VideoChecker.new("video_check.log")
+checker = VideoChecker::Checker.new("video_check.log")
 checker.check_urls(urls)
